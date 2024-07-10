bf50134e9b0339bfde8b5fdae37a947c7cab39eb
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-07-10 18:14:03 +0100

    started settings

---
+++ ui/src/routes/data/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	//
+</script>
+
+<main class="p-2">
+    <h1>Data!</h1>
+</main>
---
+++ ui/src/routes/settings/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Settings }  from '$lib/generated/index';
+	import { Card, Field, Button, Input, Icon } from 'svelte-ux'
+    let settings : Settings = {
+        urlPrefix: '/api/v1',
+        fields: []
+    }
+
+const onAddField = () => {
+    settings.fields = [...settings.fields, {
+        name: 'New Field',
+        type: 'string'
+    }]
+}
+</script>
+
+<main class="p-2">
+    <h1>Settings!</h1>
+    <pre>{JSON.stringify(settings, null, 2)}</pre>
+    <Button class="p-2" color="secondary" variant="outline" rounded on:click={onAddField}>Add</Button>
+
+    {#each settings.fields as field}
+    <div class="grid grid-cols-[auto,1fr,auto] gap-2">
+        <Card>
+            <Field label="Name" let:id>
+                <Input {id} replace="fieldname" bind:value={field.name} />
+              </Field>
+        </Card>
+        <Card>
+            <Field label="Type" let:id>
+                <Input {id} replace="type" bind:value={field.name} />
+              </Field>
+        </Card>
+    </div>
+    {/each}
+</main>
