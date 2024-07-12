44e029347840d0f77674b2859375fa9ff18fca41
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-07-12 10:24:26 +0100

    Code page

--- ui/src/routes/code/+page.svelte
+++ ui/src/routes/code/+page.svelte
@@ -1,53 +1,48 @@
 <script>
-    import { onMount } from 'svelte';
-    import * as ts from 'typescript';
-  
-    let code = `console.log("Hello, world!");`;
-    let output = '';
-  
+	import { onMount } from 'svelte'
+	import * as ts from 'typescript'
 
-    function callMe() {
-      console.log('callMe');
-    }
+	let code = `console.log("Hello, world!");`
+	let output = ''
 
-    function executeCode() {
-      try {
-        // Compile TypeScript to JavaScript
-        const compiledCode = ts.transpileModule(code, {
-          compilerOptions: { module: ts.ModuleKind.ESNext }
-        }).outputText;
-  
-        // Use a sandboxed environment to execute the code
-        const iframe = document.createElement('iframe')
-        iframe.style.display = 'none'
-        document.body.appendChild(iframe)
-        const iframeWindow = iframe.contentWindow
-        
-        iframeWindow.callMe = callMe
-        const result = iframeWindow.eval(compiledCode)
+	function callMe() {
+		console.log('callMe')
+	}
 
-        eval("console.log('Hello from eval!')")
-  
-        output = 'Code executed successfully with: ' + result;
-      } catch (error) {
-        output = `Error: ${error.message}`;
-      }
-    }
-  </script>
-  
-  <main>
+	function executeCode() {
+		try {
+			// Compile TypeScript to JavaScript
+			const compiledCode = ts.transpileModule(code, {
+				compilerOptions: { module: ts.ModuleKind.ESNext }
+			}).outputText
 
-    <textarea bind:value={code}></textarea>
-    <button on:click={executeCode}>Execute</button>
-    <p>{output}</p>
-    
-  </main>
+			// Use a sandboxed environment to execute the code
+			const iframe = document.createElement('iframe')
+			iframe.style.display = 'none'
+			document.body.appendChild(iframe)
+			const iframeWindow = iframe.contentWindow
 
+			iframeWindow.callMe = callMe
+			const result = iframeWindow.eval(compiledCode)
 
-  <style>
-    textarea {
-      width: 100%;
-      height: 150px;
-    }
-  </style>
-  
+			eval("console.log('Hello from eval!')")
+
+			output = 'Code executed successfully with: ' + result
+		} catch (error) {
+			output = `Error: ${error.message}`
+		}
+	}
+</script>
+
+<main>
+	<textarea bind:value={code}></textarea>
+	<button on:click={executeCode}>Execute</button>
+	<p>{output}</p>
+</main>
+
+<style>
+	textarea {
+		width: 100%;
+		height: 150px;
+	}
+</style>
